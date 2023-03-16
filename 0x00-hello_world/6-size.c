42199935ee6068d0a09cd7d50a675706f3fcef00
Fekoussa <nezha.fekoussa1@gmail.com>
2023-03-16 16:36:55 -0700

    my commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,6 +10,11 @@
 
 int main(void)
 {
+char charType;
+int integerType;
+long longType;
+long long longlongType;
+float floatType;
 printf("Size of a char: %lu byte(s)\n".sizeof(char));
 printf("Size of an int: %lu byte(s)\n".sizeof(int));
 printf("Size of a long int: %lu byte(s)\n".sizeof(long int));
