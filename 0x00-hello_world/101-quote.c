86fd100eeaa6ab30f4e66452085c0bfd8557dd52
Fekoussa <nezha.fekoussa1@gmail.com>
2023-03-16 17:21:24 -0700

    my commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+/**
+ * main - a C program that prints a line
+ * Descriptin : function puts line
+ * Return: 1
+ */
+int main(void)
+{
+	char string[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(STDERR_FILENO, line, sizeof(line) - 1);
+
+	return (1);
+}
