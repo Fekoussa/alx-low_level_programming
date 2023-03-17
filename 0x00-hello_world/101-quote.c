94c6f5e57dd68b97a02d8a7921e781a98d6abcac
Fekoussa <nezha.fekoussa1@gmail.com>
2023-03-17 16:03:05 -0700

    my commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,13 @@
+#include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - a C program that prints a line
- * Descriptin : function puts line
- * Return: 1
- */
+* main - Entry point
+*
+* Return: Always 1 (Success)
+*/
 int main(void)
 {
-	char string[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	write(STDERR_FILENO, line, sizeof(line) - 1);
-
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
